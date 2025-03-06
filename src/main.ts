3b2d791834dce1f2d85734c4e69e0810961bfd3d
HOT-DOG <rightl2053@gmail.com>
2025-03-06 18:10:33 +0500

    code changed and fixed

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000'],
+		origin: ['http://localhost:3000', 'https://listiify.vercel.app'],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})
